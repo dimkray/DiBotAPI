043c85f1ad6b8a18b2dbc52719e284f1ac799248
dimkray <dimkray@users.noreply.github.com>
2018-07-20 15:08:09 +0300

    settings 3

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get['SECRET_KEY','cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag']
+SECRET_KEY = os.environ.get('SECRET_KEY','cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # 'z5%&7!vt16%pveq(*^u86nq(3z76m+fs*1w+c==zz@5k_0q8h='
 
 # SECURITY WARNING: don't run with debug turned on in production!
